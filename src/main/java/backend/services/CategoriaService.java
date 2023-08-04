81a9ab3b605423ab8f559ac9248ad28dc8c59354
Julio Cesar Melegari <juliocesar.melegari@gmail.com>
2023-08-04 00:15:02 -0300

    add repository and service of categoria

---
+++ src/main/java/backend/services/CategoriaService.java
@@ -0,0 +1,22 @@
+package backend.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import backend.domain.Categoria;
+import backend.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repository;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> categoria = repository.findById(id);
+		return categoria.orElse(null);
+	}
+
+}
